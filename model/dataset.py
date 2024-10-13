ce17a9c875a2a61b88c3c764172880c08772bb28
Sera <117317257+SeraDreams@users.noreply.github.com>
2024-10-14 00:35:36 +0300

    Update dataset.py

--- model/dataset.py
+++ model/dataset.py
@@ -217,7 +217,7 @@ def load_dataset(
         print("Should manually modify the path of huggingface dataset to your need.\n" +
               "May also the corresponding script cuz different dataset may have different format.")
         # pre, post = dataset_name.split("_")
-        path = "DE/*.tar"
+        path = "EN/*.tar"
         train_dataset = HFDataset(load_dataset("amphion/Emilia-Dataset", data_files={"en": path}, split="en", streaming=True))
 
     return train_dataset
